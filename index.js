9168a6fc766213499048d53d2906025c684cddeb
grapgrap <fussaep@gmail.com>
2017-07-19 18:28:54 +0900

    login api 작성 /login?id=USER_ID&password=USER_PASSWORD

---
+++ index.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const request = require('request-promise');
+const app = express();
+
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    res.header("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
+    next();
+});
+
+app.get('/login', (req, res) => {
+    const id = req.query.id;
+    const password = req.query.password;
+    const url = `http://kumohweb.kumoh.ac.kr/mybsvr/login/login.jsp?id=${id}&passwd=${password}`;
+
+    const expressRes = res;
+
+    request(url).then(res => {
+        if (res.match('OK')) {
+            expressRes.send({
+                staus: 'success',
+                result: 'login success'
+            })
+        } else {
+            expressRes.send({
+                status: 'success',
+                result: 'login fail'
+            })
+        }
+    }).catch(error => {
+        console.log(error);
+        expressRes.send({
+            status: 'fail',
+            result: error
+        })
+    });
+});
+
+app.listen(3000, () => {
+    console.log('Server open in 3000 port');
+});
