3141a18572e212869b861f5c1928b55fd9924626
grapgrap <fussaep@gmail.com>
2017-08-10 13:28:02 +0900

    예약

--- index.js
+++ index.js
@@ -2,14 +2,17 @@ const express = require('express');
 const request = require('request-promise');
 const mysql = require('promise-mysql');
 const moment = require('moment');
+const bodyParser = require('body-parser');
 const app = express();
 
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Methods", "POST, GET");
-    res.header("Access-Control-Allow-Headers", 'Authorization');
+    res.header("Access-Control-Allow-Headers", 'Authorization, CONTENT-TYPE');
     next();
 });
+
+app.use(bodyParser.json());
 
 mysql.createConnection({
     host: 'localhost',
@@ -31,7 +34,7 @@ mysql.createConnection({
         request(url).then(res => {
             if (res.match('OK')) {
                 // 로그인 성공시 admin 체크 수행
-                const query = `SELECT * FROM admin WHERE id = ${id}`;
+                const query = `SELECT * FROM admin WHERE id = ${ mysql.escape(id) }`;
                 conn.query(query).then(rows => {
                     if (rows.length !== 0) {
                         expressRes.send({
@@ -68,7 +71,7 @@ mysql.createConnection({
             query = `SELECT booking.id as id, booker, booking_time, section.name as section 
                     FROM section, booking 
                     WHERE 
-                        booking_date = '${moment().format('YYYY-MM-DD')}' AND 
+                        booking_date = ${ mysql.escape(moment().format('YYYY-MM-DD')) } AND 
                         booking.isdelete = 0 AND 
                         section.id = booking.section`;
         } else if (req.query.date_flag === 'tomorrow') {
@@ -76,7 +79,7 @@ mysql.createConnection({
                 `SELECT booking.id as id, booker, booking_time, section.name as section 
                     FROM section, booking 
                     WHERE 
-                        booking_date = '${moment().add(1, 'd').format('YYYY-MM-DD')}' AND 
+                        booking_date = ${ mysql.escape(moment().add(1, 'd').format('YYYY-MM-DD')) } AND 
                         booking.isdelete = 0 AND 
                         section.id = booking.section`;
         }
@@ -94,18 +97,58 @@ mysql.createConnection({
     });
     // 섹션 리스트
     app.get('/section', (req, res) => {
-       const query = `SELECT name FROM section WHERE isdelete = 0`;
-       conn.query( query ).then( rows => {
-           res.send({
-               status: 'success',
-               result: rows
-           })
-       }).catch( err => {
-           res.send({
-               status: 'fail',
-               result: err
-           })
-       })
+        const query = `SELECT id, name FROM section WHERE isdelete = 0`;
+        conn.query(query).then(rows => {
+            res.send({
+                status: 'success',
+                result: rows
+            })
+        }).catch(err => {
+            res.send({
+                status: 'fail',
+                result: err
+            })
+        })
+    });
+    // 예약
+    app.post('/book', (req, res) => {
+        const data = req.body;
+        let query = `SELECT * FROM booking WHERE booking_date = ${mysql.escape(data.booking_date)} AND section = ${mysql.escape(data.section)}`;
+        conn.query(query).then(rows => { // 입력받은 예약정보의 날짜, 섹션에 해당하는 예약 정보 호출
+            console.log( rows );
+            let canInsert = true;
+            let bookingTime = data.booking_time.split(', ');
+            for (let i = 0; i < rows.length; i++) { // 입력받은 예약정보의 예약 시간과 1시간이라도 겹치는 row가 발견되면 INSERT 하지 않음.
+                if ( !canInsert ) break; // insert를 하지 못하면 loop 종료
+                let bookedTime = rows[i].booking_time.split(', ');
+                for ( let j = 0; j < bookedTime.length; j++ ) {
+                    if ( bookingTime.indexOf( bookedTime[j] ) !== -1 ) { // 겹치는 시간이 존재한다는 뜻
+                        canInsert = false; // insert 하지못함
+                        break;
+                    }
+                }
+            }
+
+            if ( canInsert ) { // 위의 확인 과정을 거쳐서 insert를 할 수 있다고 하면 Insert를 진행함.
+                let query = `INSERT INTO booking (booker, booking_time, booking_date, section) VALUES (${mysql.escape(data.booker)}, ${mysql.escape(data.booking_time)}, ${mysql.escape(data.booking_date)}, ${mysql.escape(data.section)})`;
+                conn.query(query).then(result => {
+                    res.send({
+                        status: 'success',
+                        result: 'insert success'
+                    });
+                }).catch(error => {
+                    res.send({
+                        status: 'fail',
+                        result: error
+                    });
+                });
+            } else {
+                res.send({
+                    status: 'success',
+                    result: 'insert fail, exist same booking_time'
+                });
+            }
+        });
     });
 });
 
